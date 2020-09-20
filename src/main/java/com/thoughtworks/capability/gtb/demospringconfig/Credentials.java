127fe77365ba545606484b6061f2fc389ec38ef3
xyz <xyzzhybz@163.com>
2020-09-20 20:01:56 +0800

    [xyz]finished problem1

---
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/Credentials.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+public class Credentials {
+    private String username;
+    private String password;
+    private String authMethod;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAuthMethod(String authMethod) {
+        this.authMethod = authMethod;
+    }
+
+    @Override
+    public String toString() {
+        return "Credentials{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", authMethod='" + authMethod + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,21 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+
+    private List<String> defaultRecipients;
+
+    private Map<String,Boolean> additionalHeaders;
+
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +42,27 @@ public class MailConfig {
         this.from = from;
     }
 
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public void setAdditionalHeaders(Map<String, Boolean> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + credentials +
                 '}';
     }
 }
